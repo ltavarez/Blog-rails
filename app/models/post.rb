f2aac8c02b95e01c3f9c408b3e281003b0f2cb92
GBH <gbh@gbh-MS-7788.(none)>
2013-09-30 09:33:26 -0400

    Space

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
-	has_many :comments, dependent: :destroy
-	has_many :questions, dependent: :destroy
-	accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
-	validates :title, presence: true,
+  has_many :comments, dependent: :destroy
+  has_many :questions, dependent: :destroy
+  accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+  validates :title, presence: true,
                     length: { minimum: 5 }
-    has_many :assets, :dependent => :destroy
-    accepts_nested_attributes_for :assets, :allow_destroy => true
+  has_many :assets, :dependent => :destroy
+  accepts_nested_attributes_for :assets, :allow_destroy => true
 end
